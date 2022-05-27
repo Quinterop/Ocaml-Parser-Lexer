aeb1869914e2edc36069ae706e0773fd361d89ca
sya <alassane.paris@gmail.com>
2022-05-28 01:16:10 +0200

    reprint

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 SY Alassane 
 DOROSENCO Nadejda
 
-seuls le parseur et le lexeur sont fonctionnels 
+seuls le reprint est fonctionnel
+
+ce commit date d'après minuit mais j'ai manqué de temps et j'en appelle a votre indulgence, merci d'avance
--- main.ml
+++ main.ml
@@ -5,19 +5,54 @@ let lexbuf = Lexing.from_channel (open_in (Sys.argv.(1)))
 
 let auto = Parser.axiome Lexer.next_token lexbuf
 
+let rec printcharlist l =
+  match l with
+  |[] ->  ()
+  |(a::l) -> print_char a; printcharlist l
+;;
+
+
+
+
+let transitions b = 
+  match b with
+  |(a,Some (g),c,d,Some (e)) -> print_char a ; print_char g ; print_char c ;print_char d; printcharlist e
+  |(a,None,c,d,None) -> print_char a ; print_char c ;print_char d
+  |(a,Some (g),c,d,None) -> print_char a ; print_char g ; print_char c ;print_char d; 
+  |(a,None,c,d,Some(e)) -> print_char a ; print_char c ;print_char d; printcharlist e
+;;
+
+let declarations a =
+  match a with
+  (b,c,d,e,f) ->print_string "input symbols : "; printcharlist b;print_string "\nstack symbols :"; printcharlist c; print_string "\nstates :"; printcharlist d ; print_char e ; print_char f; print_newline()
+;;
+
+let rec translists b =
+match b with
+|[] ->  ()
+|(a::l) -> transitions a ;print_newline ();translists l
+
+let testauto auto = 
+  match auto with
+  |(a,b) -> declarations a; translists b
+;;
+
+
+
+
+
+testauto auto
+
 (*
-
-
-
-let ch = open_in (Sys.argv.(1)) in
-let lb = Lexing.from_channel ch in
-try
-  while true do
-    let t = Lexer.next_token lb
-  in
-  Printf.printf "%s \n" (Token.to_string t);
-  if t=Token.EOF then exit 0
-  done
+  let ch = open_in (Sys.argv.(1)) in
+  let lb = Lexing.from_channel ch in
+  try
+    while true do
+      let t = Lexer.next_token lb
+    in
+    Printf.printf "%s \n" (Token.to_string t);
+    if t=Token.EOF then exit 0
+    done
 with _ ->
   Printf.printf "Erreur lexicale\n"
 ;;
